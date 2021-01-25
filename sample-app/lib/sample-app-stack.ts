cde5121137c36069a11bf907a8d6907daf97f8bd
Kaori Morikawa <kaori.morikawa0507@gmail.com>
2021-01-25 09:25:42 +0900

    Add Hello lambda

--- sample-app/lib/sample-app-stack.ts
+++ sample-app/lib/sample-app-stack.ts
@@ -1,8 +1,14 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class SampleAppStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X, // execution environment
+      code: lambda.Code.fromAsset('lambda'), // code loaded from lambda directory
+      handler: 'hello.handler' // file is "hello" and function is "handler"
+    });
   }
 }
